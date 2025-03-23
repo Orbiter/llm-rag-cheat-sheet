07bd86bee967284656e9f0aec16971433623b5e8
Michael Peter Christen <mc@yacy.net>
2025-03-23 17:22:45 +0100

    fixed image basic (openai and ollama was mixed up)

--- image/image_basic_ollama_api.py
+++ image/image_basic_ollama_api.py
@@ -12,21 +12,21 @@ payload = {
     "max_tokens": 1024,
     "messages": [
         {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": [
-            {"type": "text", "text": "Explain whats in the image: first write down the exact content of the image then explain it."},
-            {"type": "image_url", "image_url": {"url": f"data:image/png;base64,{base64_image}"}}
-        ]}
+        {
+            "role": "user",
+            "content": "Explain whats in the image: first write down the exact content of the image then explain it.",
+            "images": [base64_image]
+        }
     ],
     "stream": False
 }
 
 try:
     conn = http.client.HTTPConnection("localhost", 11434)
-    conn.request("POST", "/v1/chat/completions", json.dumps(payload),
+    conn.request("POST", "http://localhost:11434/api/chat", json.dumps(payload),
                  {"Content-Type": "application/json"})
     response = conn.getresponse()
     response_text = response.read().decode()
-    print("Response text read:" + response_text)
     response_json = json.loads(response_text)
     print(json.dumps(response_json, indent=2))
 except Exception as e:
