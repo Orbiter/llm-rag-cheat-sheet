bf12bc4dadb535f94f0a2434bd57da706d608f00
Michael Peter Christen <mc@yacy.net>
2025-05-04 18:24:06 +0200

    tool call example

---
+++ tools/chat_curl_tool.sh
@@ -0,0 +1,27 @@
+# run with ./chat_curl_tool.sh  | python3 -m json.tool
+curl -X POST "http://localhost:11434/v1/chat/completions"\
+     -s -H "Content-Type: application/json"\
+     -d '{
+    "model": "llama3.2", "temperature": 0.1, "max_tokens": 1024,
+    "messages": [
+      {"role": "system", "content": "You are a home assistant."},
+      {"role": "user", "content": "Switch on the light"}
+    ],
+    "tools": [{
+      "type": "function",
+      "function": {
+        "name": "lightswitch",
+        "description": "With this tool you can switch on the light",
+        "parameters": {
+          "type": "object",
+          "properties": {
+            "switch": { "type": "boolean", "description": "true for on, false for off" }
+          },
+          "required": [ "switch" ],
+          "additionalProperties": false
+        },
+        "strict": true
+      }
+     }],
+    "stream": false
+  }'
