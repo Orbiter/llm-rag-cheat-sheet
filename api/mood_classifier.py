e0d07f996a331a116438663a286ec41e734794f0
Michael Peter Christen <mc@yacy.net>
2025-12-08 09:52:36 +0100

    fixed mood classifier

--- api/mood_classifier.py
+++ api/mood_classifier.py
@@ -5,13 +5,14 @@ def mood_classifier(text):
     format = {
         "title": "Classifier", "type": "object",
         "properties":
-        {"mood":{ "type": "literal", "enum": ["surprised", "angry", "happy"]}}
+        {"mood":{ "type": "literal", "enum": ["surprised", "angry", "happy"]}},
+        "required": ["mood"]
     }
     messages = [
         ("system", "You are a mood classifier. Identify the mood of the request."),
         ("human", text),
     ]
-    llm = ChatOllama(base_url="http://localhost:11434", model = "llama3.2",
+    llm = ChatOllama(base_url="http://localhost:11434", model = "llama3.2:3b",
                      temperature = 0.1, num_predict = 1024, format = format)
     llm = llm.with_structured_output(schema = format, include_raw = True)
     mood = json.loads(llm.invoke(messages)['raw'].content)['mood']
@@ -22,4 +23,5 @@ def print_mood_classifier(text):
 
 print_mood_classifier("I love programming.")
 print_mood_classifier("I hate programming.")
-print_mood_classifier("I found a solution!")
+print_mood_classifier("That worked perfectly.")
+
